529355a710fc07fc83bed1f642fd2513f84dc29f
koshyanand <koshyanand@gmail.com>
2021-01-13 15:17:19 -0800

    Completed TwoPointer: 1:Q2

--- S30/TwoPointer/1/Problem2.java
+++ S30/TwoPointer/1/Problem2.java
@@ -1,1 +1,45 @@
 // Link: https://leetcode.com/problems/3sum/
+
+// Time Complexity: O(N^2)
+// Time Complexity: O(N log(N)) // For the sorting algorithm
+
+
+class Solution {
+    
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (nums.length < 3)
+            return result;
+        
+        Arrays.sort(nums);
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            
+            int left = i + 1, right = nums.length - 1, target = -nums[i];
+            while(left < right) {
+                
+                if (left - i >= 2 && nums[left] == nums[left - 1]) {
+                    left++;
+                    continue;
+                }
+
+                int total = nums[left] + nums[right];
+                
+                if (total == target) {
+                    result.add(new ArrayList<Integer>(Arrays.asList(nums[i], nums[left], nums[right])));
+                    left++;
+                    right--;
+                } else {
+                    if (total > target)
+                        right--;
+                    else
+                        left++;
+                }
+                
+            }
+        }
+        return result;
+    }
+}
