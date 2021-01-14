dcb754f5ac97baeb1f072682b00fdc0df4a12f55
koshyanand <koshyanand@gmail.com>
2021-01-13 17:14:09 -0800

    Completed TwoPointer: 1:Q3

--- S30/TwoPointer/1/Problem3.java
+++ S30/TwoPointer/1/Problem3.java
@@ -1,1 +1,24 @@
 // Link: https://leetcode.com/problems/container-with-most-water/
+
+// Time Complexity: O(N)
+// Time Complexity: O(1)
+
+class Solution {
+    public int maxArea(int[] height) {
+        
+        if (height.length < 2)
+            return 0;
+        
+        int left = 0, right = height.length - 1;
+        int maxWater = 0;
+        while(left < right) {
+            
+            maxWater = Math.max(Math.min(height[left], height[right]) * (right - left), maxWater);
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+        return maxWater;
+    }
+}
